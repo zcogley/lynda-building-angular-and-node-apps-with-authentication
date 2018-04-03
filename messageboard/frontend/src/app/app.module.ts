b95d3674c8f4666198e7818c737500812745d1ef
Zac Cogley <zaccogley@gmail.com>
2018-04-03 14:14:59 -0500

    complete chapter 2

--- messageboard/frontend/src/app/app.module.ts
+++ messageboard/frontend/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule, 
+  MatCardModule,
+  MatInputModule,
+  MatSnackBarModule,
+  MatToolbarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { MessagesComponent } from './messages.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule, 
+    MatCardModule,
+    MatInputModule,
+    MatSnackBarModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ messageboard/frontend/src/app/messages.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'messages',
+    template: `
+    <div *ngFor="let message of messages">
+        <mat-card style="margin:8px">
+        <mat-card-title> {{message.owner}} </mat-card-title> 
+        <mat-card-content> {{message.text}} </mat-card-content>
+        </mat-card>
+    </div>
+    `
+})
+export class MessagesComponent {
+    messages = [{text: 'some text', owner: 'Tim'}, {text: 'other message', owner: 'Jane'}];
+}
